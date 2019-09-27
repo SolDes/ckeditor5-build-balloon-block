90a3023d0c10195b178fb8b0ef7e13b9376b4728
Jonathan Whitney <jonathan.whitney@salesforce.com>
2019-09-27 16:51:00 -0400

    Added additional plugins to build, added default editor configuration

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -7,14 +7,16 @@
 import BalloonEditorBase from '@ckeditor/ckeditor5-editor-balloon/src/ballooneditor';
 
 import Essentials from '@ckeditor/ckeditor5-essentials/src/essentials';
-import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
+// import UploadAdapter from '@ckeditor/ckeditor5-adapter-ckfinder/src/uploadadapter';
 import Autoformat from '@ckeditor/ckeditor5-autoformat/src/autoformat';
 import BlockToolbar from '@ckeditor/ckeditor5-ui/src/toolbar/block/blocktoolbar';
 import Bold from '@ckeditor/ckeditor5-basic-styles/src/bold';
 import Italic from '@ckeditor/ckeditor5-basic-styles/src/italic';
-import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
-import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
-import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
+import Underline from '@ckeditor/ckeditor5-basic-styles/src/underline';
+import Code from '@ckeditor/ckeditor5-basic-styles/src/code';
+// import BlockQuote from '@ckeditor/ckeditor5-block-quote/src/blockquote';
+// import CKFinder from '@ckeditor/ckeditor5-ckfinder/src/ckfinder';
+// import EasyImage from '@ckeditor/ckeditor5-easy-image/src/easyimage';
 import Heading from '@ckeditor/ckeditor5-heading/src/heading';
 import Image from '@ckeditor/ckeditor5-image/src/image';
 import ImageCaption from '@ckeditor/ckeditor5-image/src/imagecaption';
@@ -36,14 +38,16 @@ export default class BalloonEditor extends BalloonEditorBase { }
 // Plugins to include in the build.
 BalloonEditor.builtinPlugins = [
 	Essentials,
-	UploadAdapter,
+	// UploadAdapter,
 	Autoformat,
 	BlockToolbar,
 	Bold,
 	Italic,
-	BlockQuote,
-	CKFinder,
-	EasyImage,
+	Underline,
+	Code,
+	// BlockQuote,
+	// CKFinder,
+	// EasyImage,
 	Heading,
 	Image,
 	ImageCaption,
@@ -63,30 +67,91 @@ BalloonEditor.builtinPlugins = [
 BalloonEditor.defaultConfig = {
 	blockToolbar: [
 		'heading',
+		'bold',
+		'italic',
+		'underline',
+		'code',
 		'|',
 		'bulletedList',
 		'numberedList',
-		'imageUpload',
-		'blockQuote',
-		'insertTable',
-		'mediaEmbed',
 		'|',
-		'undo',
-		'redo'
+		'ImageUpload',
+		'MediaEmbed',
+		'|',
+		'InsertTable',
+		'|',
+		'Undo',
+		'Redo'
 	],
 	toolbar: {
 		items: [
 			'bold',
 			'italic',
-			'link'
+			'link',
+			'Code'
+		]
+	},
+	heading: {
+		options: [
+			{
+				model: 'sldsHeadingLarge',
+				view: {
+					name: 'div',
+					classes: 'slds-text-heading_large'
+				},
+				title: 'SLDS Heading Large',
+				class: 'ck-heading_slds-text-heading-large',
+				converterPriority: 'high'
+			},
+			{
+				model: 'sldsHeadingMedium',
+				view: {
+					name: 'div',
+					classes: 'slds-text-heading_medium'
+				},
+				title: 'SLDS Heading Medium',
+				class: 'ck-heading_slds-text-heading-medium',
+				converterPriority: 'high'
+			},
+			{
+				model: 'sldsHeadingSmall',
+				view: {
+					name: 'div',
+					classes: 'slds-text-heading_small'
+				},
+				title: 'SLDS Heading Small',
+				class: 'ck-heading_slds-text-heading-small',
+				converterPriority: 'high'
+			},
+			{
+				model: 'paragraph',
+				title: 'Paragraph',
+				class: 'ck-heading_paragraph'
+			},
+			{
+				model: 'sldsTextBodySmall',
+				view: {
+					name: 'p',
+					classes: 'slds-text-body_small'
+				},
+				title: 'SLDS Text Body Small',
+				class: 'ck-heading_slds-text-body-small',
+				converterPriority: 'high'
+			}
 		]
 	},
 	image: {
 		toolbar: [
-			'imageStyle:full',
-			'imageStyle:side',
+			'imageTextAlternative',
 			'|',
-			'imageTextAlternative'
+			'imageStyle:alignLeft',
+			'imageStyle:full',
+			'imageStyle:alignRight'
+		],
+		styles: [
+			'full',
+			'alignLeft',
+			'alignRight'
 		]
 	},
 	table: {
